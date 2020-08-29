8e28ba49b7219944fb0d8bfa96cd5ea22f8d4b50
Jeff Kielman <info@jeffkielman.com>
2020-08-29 11:13:39 -0400

    Added svg rect

--- svgmove.js
+++ svgmove.js
@@ -1,30 +1,41 @@
-    (function () {
-      'use strict';
+(function () {
+  'use strict';
 
-      window.svgMove = (options) => {
+  window.svgMove = (options) => {
 
-        window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
 
-          options = options || {};
+      options = options || {};
 
-          const DOM = {
-            docElST: document.documentElement.scrollTop,
-            docElSH: document.documentElement.scrollHeight,
-            docBST: document.body.scrollTop,
-            docElCH: document.documentElement.clientHeight,
-            svgPath: document.querySelector(options.path),
-            svgMove: document.querySelector(options.moveEL)
-          }
-
-          let scrollP = (DOM.docElST + DOM.docBST) / (DOM.docElSH - DOM.docElCH);
-          let pathL = DOM.svgPath.getTotalLength();
-          let mPos = DOM.svgPath.getPointAtLength(scrollP * pathL);
-
-          DOM.svgMove.setAttribute('cy', mPos.y);
-          DOM.svgMove.setAttribute('cx', mPos.x);
-
-        });
-
+      const DOM = {
+        docElST: document.documentElement.scrollTop,
+        docElSH: document.documentElement.scrollHeight,
+        docBST: document.body.scrollTop,
+        docElCH: document.documentElement.clientHeight,
+        svgPath: document.querySelector(options.pathEl),
+        svgMove: document.querySelector(options.moveEL),
       }
 
-    })();
+      let svgMoveAxis = DOM.svgMove.nodeName;
+      let scrollP = (DOM.docElST + DOM.docBST) / (DOM.docElSH - DOM.docElCH);
+      let pathL = DOM.svgPath.getTotalLength();
+      let mPos = DOM.svgPath.getPointAtLength(scrollP * pathL);
+
+      switch (svgMoveAxis) {
+        case 'rect':
+          DOM.svgMove.setAttribute('y', mPos.y);
+          DOM.svgMove.setAttribute('x', mPos.x);
+          break;
+        case 'ellipse':
+          DOM.svgMove.setAttribute('cy', mPos.y);
+          DOM.svgMove.setAttribute('cx', mPos.x);
+          break;
+        case 'circle':
+          DOM.svgMove.setAttribute('cy', mPos.y);
+          DOM.svgMove.setAttribute('cx', mPos.x);
+      }
+    });
+
+  }
+
+})();
